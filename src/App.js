b6719325798aad3110b10223ed0bf9280019dc9f
Tomer <carollatomer@gmail.com>
2022-11-16 14:25:44 +0200

    add router and firebase

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import Categories from "./components/categories/categories.component";
+import Main from "./routes/main/main.component";
+import {Route, Routes} from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+
+
+const Shop = () => {
+    return (
+        <div>
+            Shop Component
+        </div>
+    )
+}
 
 const App = () => {
-    const categories = [
-        {
-            id: 1,
-            title: 'Toys',
-            imageUrl: 'https://images.dog.ceo/breeds/pembroke/n02113023_1571.jpg',
-        },
-        {
-            id: 2,
-            title: 'Food',
-            imageUrl: 'https://images.dog.ceo/breeds/entlebucher/n02108000_811.jpg',
-        },
-        {
-            id: 3,
-            title: 'Snacks',
-            imageUrl: 'https://images.dog.ceo/breeds/malinois/n02105162_6501.jpg',
-        },
-        {
-            id: 4,
-            title: 'Clothes',
-            imageUrl: 'https://images.dog.ceo/breeds/clumber/n02101556_823.jpg',
-        },
-        {
-            id: 5,
-            title: 'Accessories',
-            imageUrl: 'https://images.dog.ceo/breeds/appenzeller/n02107908_2151.jpg',
-        },
-    ];
-
     return (
-        <Categories categories={categories} />
+        <Routes>
+            <Route path="/" element={<Navigation />}>
+                <Route index element={<Main/>} />
+                <Route path="shop" element={<Shop/>} />
+            </Route>
+        </Routes>
     );
 }
 
