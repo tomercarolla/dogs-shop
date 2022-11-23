78323cecf0e7e037b20db3ff5a3c5ea26e42b206
Tomer <carollatomer@gmail.com>
2022-11-23 18:37:57 +0200

    add router in shop page and navigate to category product page by category

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
 
 const App = () => {
+
     return (
         <Routes>
             <Route path="/" element={<Navigation />}>
                 <Route index element={<Main/>} />
-                <Route path="shop" element={<Shop/>} />
+                <Route path="shop/*" element={<Shop/>} />
                 <Route path="auth" element={<Authentication/>} />
                 <Route path="checkout" element={<Checkout/>} />
             </Route>
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,32 +1,13 @@
-import {Fragment, useContext} from "react";
-import {CategoriesContext} from "../../context/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
-
-import './shop.styles.scss';
+import {Route, Routes} from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 const Shop = () => {
-
-    const {categoriesMap} = useContext(CategoriesContext);
-
     return (
-        <Fragment>
-            {
-                Object.keys(categoriesMap).map((title) => (
-                    <Fragment key={title}>
-                        <h2>{title}</h2>
-                        <div className="products-container">
-                            {
-                                categoriesMap[title].map(product => {
-                                    return (
-                                        <ProductCard key={product.id} product={product}></ProductCard>
-                                    )
-                                })
-                            }
-                        </div>
-                    </Fragment>
-                ))
-            }
-        </Fragment>
+        <Routes>
+            <Route index element={<CategoriesPreview/>} />
+            <Route path=":category" element={<Category/>}/>
+        </Routes>
     )
 }
 
